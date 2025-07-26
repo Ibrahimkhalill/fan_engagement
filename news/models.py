e0840bb27f51f8402faf0b1ea783e9b241d08b8b
Ibrahim Khalil <mdibrahimkhalil516@gmail.com>
2025-07-26 16:53:57 +0600

    Alter title field in News model to use TextField for better content handling

--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ from ckeditor.fields import RichTextField
 
 class News(models.Model):
     image = models.ImageField(upload_to='news/', blank=True, null=True)
-    title = models.CharField(max_length=200)
+    title = models.TextField(blank=True, null=True)
     description = RichTextField(blank=True, null=True)
     upload_date = models.DateTimeField(auto_now_add=True)
 
